117c33adc5290bd979b0d9deeb1f33d2f2b44fe6
Josh Haines <Josh@JoshHaines.com>
2022-01-03 17:08:42 -0500

    update ts bowling

--- bowling/code.test.ts
+++ bowling/code.test.ts
@@ -1,22 +1,74 @@
-import { summer, BowlingGame } from './code'
+import { addTwoNumbers, BowlingGame } from "./code"
 
-describe("Test summer function to validate jest", () => {
-    it('checks sum of 1 & 2', () => {
-      expect(summer(1, 2)).toBe(3);
-    })
-});
+describe("Test addTwoNumbers function to validate jest", () => {
+  test("checks sum of 1 & 2", () => {
+    expect(addTwoNumbers(1, 2)).toBe(3)
+  })
+})
 
 describe("test BowlingGame", () => {
-    it ('instantiates new class', () => {
-       let b = new BowlingGame([1,2,3])
-       expect(b.rolls).toStrictEqual([1,2,3])
-    })
-    it('checks for too many balls thrown', () => {
-        const rolls = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21]
-        expect(BowlingGame(rolls)).toThrow('Too Many rolls, maximum of 20 balls allowed.')
-    })
-    it('checks score for 3 balls', () => {
-        let b = new BowlingGame([1,1,1])
-        expect(b.score()).toBe(3)
-    })
+  test("instantiates new game with partial rolls", () => {
+    let b = new BowlingGame([1, 2, 3])
+    expect(b.rolls).toStrictEqual([1, 2, 3])
+  })
+
+  test("instantiates new empty game", () => {
+    let b = new BowlingGame([])
+    expect(b.rolls).toStrictEqual([])
+    expect(b.score().score).toBe(0)
+  })
+
+  test("checks for too many balls thrown", () => {
+    const rolls = [
+      1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10,
+      10,
+    ]
+    expect(() => {
+      new BowlingGame(rolls)
+    }).toThrow("Too many rolls, maximum of 21 balls allowed.")
+  })
+
+  test("checks for too too many pins on a single roll", () => {
+    const rolls = [1, 2, 9, 10, 11]
+    expect(() => {
+      new BowlingGame(rolls)
+    }).toThrow("Invalid pin count provided")
+  })
+
+  test("checks for full game given (21 balls)", () => {
+    const rolls = [
+      1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10,
+    ]
+    let b = new BowlingGame(rolls)
+    expect(b.score().score).toBe(165)
+    expect(b.score().message).toBe("21 balls thrown so far.")
+  })
+
+  test("checks score for 3 balls", () => {
+    let b = new BowlingGame([1, 1, 1])
+    expect(b.score().score).toBe(3)
+    expect(b.score().message).toBe("3 balls thrown so far.")
+  })
+
+  test("zero game score of 0", () => {
+    let b = new BowlingGame([])
+    b.rollMany(20, 0)
+    expect(b.score().score).toBe(0)
+    expect(b.rolls).toStrictEqual([
+      0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+    ])
+  })
+
+  test("all 1s game - score of 20", () => {
+    let b = new BowlingGame([])
+    b.rollMany(20, 1)
+    expect(b.score().score).toBe(20)
+  })
+
+  test("all 9s game - score of 180", () => {
+    let b = new BowlingGame([])
+    b.rollMany(20, 9)
+    expect(b.score().score).toBe(180)
+    expect(b.score().message).toBe("20 balls thrown so far.")
+  })
 })
--- bowling/code.ts
+++ bowling/code.ts
@@ -1,22 +1,42 @@
-export function summer(x: number, y: number) {
-    return x + y
+import { stringify } from "ts-jest"
+
+export function addTwoNumbers(x: number, y: number) {
+  return x + y
 }
 
-function sum(prev: number, next: number) {
-    return prev + next
+export class BowlingGame {
+  rolls: number[]
+
+  constructor(rollArray: number[]) {
+    // invalid rolls
+    if (rollArray.length > 21) {
+      throw "Too many rolls, maximum of 21 balls allowed."
+    }
+    // more than 10 pins for a single roll
+    if (rollArray.some((el) => el > 10)) {
+      throw "Invalid pin count provided."
+    }
+    this.rolls = rollArray
+  }
+  score(): {
+    score: number
+    message: string
+  } {
+    if (this.rolls.length == 0) {
+      return {
+        score: 0,
+        message: "No balls thrown yet.",
+      }
+    } else {
+      return {
+        score: this.rolls.reduce(addTwoNumbers),
+        message: String(this.rolls.length) + " balls thrown so far.",
+      }
+    }
+  }
+  rollMany(balls: number, pins: number) {
+    for (let i = 0; i < balls; i++) {
+      this.rolls?.push(pins)
+    }
+  }
 }
-
-export class BowlingGame{
-    rolls: number[]
-
-    constructor(rollArray: number[]) {
-        if (rollArray.length >20) {
-            throw('Too many rolls, maximum of 20 balls allowed.')
-        } else {
-            this.rolls = rollArray
-        }
-    }
-    score() {
-        return this.rolls.reduce(sum)
-    }
-};
